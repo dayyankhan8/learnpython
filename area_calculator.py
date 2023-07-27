446493efb4c371bc8fb6eaffcb803a23fb9b597c
dayyankhan8 <138666830+dayyankhan8@users.noreply.github.com>
2023-07-27 22:47:57 +0500

    Update area_calculator.py
    added function for sqaure

--- area_calculator.py
+++ area_calculator.py
@@ -4,13 +4,16 @@ print("2. Rectangle")
 print("3. Triangle")
 print("4. Circle")
 
+def area_of_square(length):
+    return length * length
+    
 user_option = int(input("Please enter your choice: "))
-if user_option==1:
-    length_of_square=float(input("Enter the length of the square: "))
-    print(length_of_square*length_of_square)
-elif user_option==2:
-    length_of_rectangle=float(input("Enter the length of rectangle: "))
-    width_of_rectangle=float(input("Enter the width of rectangle: "))
+if user_option == 1:
+    length_of_square = float(input("Enter the length of the square: "))
+    print(area_of_square(length_of_square))
+elif user_option == 2:
+    length_of_rectangle = float(input("Enter the length of rectangle: "))
+    width_of_rectangle = float(input("Enter the width of rectangle: "))
     print(length_of_rectangle*width_of_rectangle)
 elif user_option==3:
     base_of_triangle=float(input("Enter the base of triangle: "))
